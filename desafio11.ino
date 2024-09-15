4f98ed507d3c886e82bf1063215d29acc1d4f331
Juanchoski <judach2006@gmail.com>
2024-09-14 22:44:53 -0500

    arreglo de error de impresion en la lcd

--- desafio11.ino
+++ desafio11.ino
@@ -1,13 +1,20 @@
-#include <LiquidCrystal_I2C.h>
+#include <Adafruit_LiquidCrystal.h>
+
 int pinA = A0;
 int valor = 0;
 int pulsadorI = 2;
 int pulsadorF = 3;
+Adafruit_LiquidCrystal lcd(0);
+
 void setup (){
-  pinMode(pulsadorI,IMPUT);
-  pinMode(pulsadorF,IMPUT);
+  pinMode(pulsadorI,INPUT);
+  pinMode(pulsadorF,INPUT);
   Serial.begin(9600);
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+  lcd.print("Bienvenido");
 } 
+
 void loop(){
   valor = analogRead(pinA);
   Serial.println(valor);
